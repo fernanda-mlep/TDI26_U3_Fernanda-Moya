96065ad9c2d24efcdffa5e94813f5e35eb0ab1bf
fernanda-mlep <fernanda.moya.l@mail.pucv.cl>
2026-06-09 16:55:48 -0400

    Cursor interactivo
    Avance de cursor interactivo antes de las 17:00 hrs

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Configuración del efecto máquina de escribir
 const textoEscribir = "Catapultazo";
-const velocidadEscritura = 175; // Tiempo en milisegundos entre cada letra (menor número = más rápido)
+const velocidadEscritura = 175; 
 
 let indiceActual = 0;
 const contenedorTexto = document.getElementById("typer");
@@ -8,20 +8,25 @@ const boton = document.getElementById("btn-comenzar");
 
 function escribirLetra() {
     if (indiceActual < textoEscribir.length) {
-        // Agrega la siguiente letra
         contenedorTexto.textContent += textoEscribir.charAt(indiceActual);
         indiceActual++;
-        // Llama a la función de nuevo tras el delay configurado
         setTimeout(escribirLetra, velocidadEscritura);
     } else {
-        // Al terminar de escribir, muestra el botón '+' tras una breve pausa de 300ms
         setTimeout(() => {
             boton.classList.add("mostrar");
         }, 300); 
     }
 }
 
-// Iniciar la animación automáticamente con un retraso de 1 segundo (1000ms) al abrir el sitio web
 window.addEventListener("DOMContentLoaded", () => {
     setTimeout(escribirLetra, 1000); 
+});
+
+
+ // Cursor personalizado 
+const cursorEspecial = document.querySelector(".custom-cursor");
+
+window.addEventListener("mousemove", (e) => {
+    cursorEspecial.style.left = e.clientX + "px";
+    cursorEspecial.style.top = e.clientY + "px";
 });
